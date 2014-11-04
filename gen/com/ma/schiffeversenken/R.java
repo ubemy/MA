2edaa1a54b0768576e4124297cb86668207d0b31
Schlender <kschlender@fh-bielefeld.de>
2014-11-04 12:27:46 +0100

    expand Kreuzer

--- gen/com/ma/schiffeversenken/R.java
+++ gen/com/ma/schiffeversenken/R.java
@@ -127,22 +127,6 @@ public final class R {
         public static final int startseite=0x7f090005;
     }
     public static final class string {
-<<<<<<< HEAD
-        public static final int abc_action_bar_home_description=0x7f070008;
-        public static final int abc_action_bar_up_description=0x7f070009;
-        public static final int abc_action_menu_overflow_description=0x7f07000a;
-        public static final int abc_action_mode_done=0x7f070007;
-        public static final int abc_activity_chooser_view_see_all=0x7f070011;
-        public static final int abc_activitychooserview_choose_application=0x7f070010;
-        public static final int abc_searchview_description_clear=0x7f07000d;
-        public static final int abc_searchview_description_query=0x7f07000c;
-        public static final int abc_searchview_description_search=0x7f07000b;
-        public static final int abc_searchview_description_submit=0x7f07000e;
-        public static final int abc_searchview_description_voice=0x7f07000f;
-        public static final int abc_shareactionprovider_share_with=0x7f070013;
-        public static final int abc_shareactionprovider_share_with_application=0x7f070012;
-=======
->>>>>>> refs/remotes/origin/master
         public static final int action_settings=0x7f070001;
         public static final int app_name=0x7f070000;
         public static final int hello_world=0x7f070003;
--- src/com/ma/schiffeversenken/model/Kreuzer.java
+++ src/com/ma/schiffeversenken/model/Kreuzer.java
@@ -1,5 +1,6 @@
 package com.ma.schiffeversenken.model;
 
 public class Kreuzer implements Schiff {
+	String name = "Kreuzer";
 
 }

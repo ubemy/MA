27ef66510d1c4f55c7e2026b18222d7c31edca51
ubemy <ubemy@hotmail.de>
2014-11-04 12:10:01 +0100

    Test

--- gen/com/ma/schiffeversenken/R.java
+++ gen/com/ma/schiffeversenken/R.java
@@ -109,36 +109,39 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_einstellungen=0x7f030000;
-        public static final int activity_main=0x7f030001;
-        public static final int activity_schwierigkeitsstufe=0x7f030002;
-        public static final int activity_spiel_starten=0x7f030003;
-        public static final int activity_startseite=0x7f030004;
+        public static final int activity_hilfe=0x7f030001;
+        public static final int activity_main=0x7f030002;
+        public static final int activity_schwierigkeitsstufe=0x7f030003;
+        public static final int activity_spiel_starten=0x7f030004;
+        public static final int activity_startseite=0x7f030005;
     }
     public static final class menu {
         public static final int einstellungen=0x7f090000;
-        public static final int main=0x7f090001;
-        public static final int schwierigkeitsstufe=0x7f090002;
-        public static final int spiel_starten=0x7f090003;
-        public static final int startseite=0x7f090004;
+        public static final int hilfe=0x7f090001;
+        public static final int main=0x7f090002;
+        public static final int schwierigkeitsstufe=0x7f090003;
+        public static final int spiel_starten=0x7f090004;
+        public static final int startseite=0x7f090005;
     }
     public static final class string {
-        public static final int abc_action_bar_home_description=0x7f070007;
-        public static final int abc_action_bar_up_description=0x7f070008;
-        public static final int abc_action_menu_overflow_description=0x7f070009;
-        public static final int abc_action_mode_done=0x7f070006;
-        public static final int abc_activity_chooser_view_see_all=0x7f070010;
-        public static final int abc_activitychooserview_choose_application=0x7f07000f;
-        public static final int abc_searchview_description_clear=0x7f07000c;
-        public static final int abc_searchview_description_query=0x7f07000b;
-        public static final int abc_searchview_description_search=0x7f07000a;
-        public static final int abc_searchview_description_submit=0x7f07000d;
-        public static final int abc_searchview_description_voice=0x7f07000e;
-        public static final int abc_shareactionprovider_share_with=0x7f070012;
-        public static final int abc_shareactionprovider_share_with_application=0x7f070011;
+        public static final int abc_action_bar_home_description=0x7f070008;
+        public static final int abc_action_bar_up_description=0x7f070009;
+        public static final int abc_action_menu_overflow_description=0x7f07000a;
+        public static final int abc_action_mode_done=0x7f070007;
+        public static final int abc_activity_chooser_view_see_all=0x7f070011;
+        public static final int abc_activitychooserview_choose_application=0x7f070010;
+        public static final int abc_searchview_description_clear=0x7f07000d;
+        public static final int abc_searchview_description_query=0x7f07000c;
+        public static final int abc_searchview_description_search=0x7f07000b;
+        public static final int abc_searchview_description_submit=0x7f07000e;
+        public static final int abc_searchview_description_voice=0x7f07000f;
+        public static final int abc_shareactionprovider_share_with=0x7f070013;
+        public static final int abc_shareactionprovider_share_with_application=0x7f070012;
         public static final int action_settings=0x7f070001;
         public static final int app_name=0x7f070000;
         public static final int hello_world=0x7f070003;
         public static final int title_activity_einstellungen=0x7f070004;
+        public static final int title_activity_hilfe=0x7f070006;
         public static final int title_activity_schwierigkeitsstufe=0x7f070005;
         public static final int title_activity_spiel_starten=0x7f070002;
     }

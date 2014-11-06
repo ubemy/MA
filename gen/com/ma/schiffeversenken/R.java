d6ab0e4cb86a02dc01109baca54ff95543dc2d97
Schlender <kschlender@fh-bielefeld.de>
2014-11-06 08:51:50 +0100

    add some help content

--- gen/com/ma/schiffeversenken/R.java
+++ gen/com/ma/schiffeversenken/R.java
@@ -138,12 +138,13 @@ public final class R {
         public static final int action_settings=0x7f070001;
         public static final int app_name=0x7f070000;
         public static final int hello_world=0x7f070003;
-        public static final int title_activity_einstellungen=0x7f070004;
-        public static final int title_activity_hilfe=0x7f070006;
-        public static final int title_activity_schwierigkeitsstufe=0x7f070005;
+        public static final int help=0x7f070004;
+        public static final int title_activity_einstellungen=0x7f070005;
+        public static final int title_activity_hilfe=0x7f070007;
+        public static final int title_activity_schwierigkeitsstufe=0x7f070006;
         public static final int title_activity_spiel_starten=0x7f070002;
-        public static final int title_activity_spielermodus=0x7f070007;
-        public static final int title_activity_test_ausgabe=0x7f070008;
+        public static final int title_activity_spielermodus=0x7f070008;
+        public static final int title_activity_test_ausgabe=0x7f070009;
     }
     public static final class style {
         /** 

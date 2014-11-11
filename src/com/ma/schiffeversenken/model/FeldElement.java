65b58a5ad83a6f6a7d379fe627ea77584894d402
ubemy <ubemy@hotmail.de>
2014-11-11 16:35:26 +0100

    Anfang Implementierung Klasse Computer

--- src/com/ma/schiffeversenken/model/FeldElement.java
+++ src/com/ma/schiffeversenken/model/FeldElement.java
@@ -1,15 +1,65 @@
 package com.ma.schiffeversenken.model;
 
 public class FeldElement {
-	//Direkte Nachbar dieses Feldes
-	FeldElement lNachbar, rNachbar, oNachbar, uNachbar;
-	//Schiff, das auf diesem FeldElement steht
-	Schiff platziertesSchiff;
+	/*
+	 * Das Spielfeld besteht aus 100 FeldElementen
+	 */
+	FeldElement lNachbar, rNachbar, oNachbar, uNachbar; //Direkte Nachbar dieses Feldes
+	Schiff platziertesSchiff; //Schiff, das auf diesem FeldElement steht
 	int id;
 	boolean belegt;
 	int kante1=0;
 	int kante2=0;
+	private boolean attackedByFirstPlayer;
+	private boolean attackedBySecondPlayer;
+	private boolean shipDestroyedByFirstPlayer;
+	private boolean shipDestroyedBySecondPlayer;
 	
+	public FeldElement(int id){
+		this.id=id;
+		this.belegt = false;
+		this.attackedByFirstPlayer = false;
+		this.attackedBySecondPlayer = false;
+		this.shipDestroyedByFirstPlayer = false;
+		this.shipDestroyedBySecondPlayer = false;
+	}
+	
+	public void attack(){
+		//Dieses FeldElement attackieren
+	}
+	
+	public boolean getAttackedByFirstPlayer(){
+		return this.attackedByFirstPlayer;
+	}
+	
+	public void setAttackedByFirstPlayer(boolean attacked){
+		this.attackedByFirstPlayer = attacked;
+	}
+	
+	public boolean getAttackedBySecondPlayer(){
+		return this.attackedBySecondPlayer;
+	}
+	
+	public void setAttackedBySecondPlayer(boolean attacked){
+		this.attackedBySecondPlayer = attacked;
+	}
+	//
+	public boolean getShipDestroyedByFirstPlayer(){
+		return this.shipDestroyedByFirstPlayer;
+	}
+	
+	public void setShipDestroyedByFirstPlayer(boolean attacked){
+		this.shipDestroyedByFirstPlayer = attacked;
+	}
+	
+	public boolean getShipDestroyedBySecondPlayer(){
+		return this.shipDestroyedBySecondPlayer;
+	}
+	
+	public void setShipDestroyedBySecondPlayer(boolean attacked){
+		this.shipDestroyedBySecondPlayer = attacked;
+	}
+	//
 	public void setKante(int kante, int wert){
 		if(kante == 1) this.kante1 = wert;
 		else if(kante == 2) this.kante2 = wert;
@@ -22,14 +72,9 @@ public class FeldElement {
 		
 		return ret;
 	}
-	
-	public FeldElement(int id){
-		this.id=id;
-		this.belegt = false;
-	}
 
 	public void setNeighbors(FeldElement lNachbar, FeldElement rNachbar, FeldElement oNachbar, FeldElement uNachbar){
-		//Weißt diesem FeldElement seine direkten Nachbarn zu
+		//Weist diesem FeldElement seine direkten Nachbarn zu
 		this.lNachbar = lNachbar;
 		this.rNachbar = rNachbar;
 		this.oNachbar = oNachbar;

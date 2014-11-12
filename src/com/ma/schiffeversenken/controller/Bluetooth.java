c1100d1722d5f35c2c3b376be241dd3dad20766a
ubemy <ubemy@hotmail.de>
2014-11-12 21:18:21 +0100

    Bluetooth Aenderungen

--- src/com/ma/schiffeversenken/controller/Bluetooth.java
+++ src/com/ma/schiffeversenken/controller/Bluetooth.java
@@ -1,15 +1,43 @@
 package com.ma.schiffeversenken.controller;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.location.LocationManager;
 
-public class Bluetooth {
+public class Bluetooth extends Activity {
 	private BluetoothAdapter bluetoothAdapter;
+	private final BluetoothManager bluetoothManager;
 	
 	public Bluetooth(){
-		bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+		bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+		bluetoothAdapter = bluetoothManager.getAdapter();
+	}
+	
+	public int blutoothOK(){
+		/*
+		 * Prueft ob Bluetooth verfuegbar und enabled ist
+		 * 
+		 * Bluetooth kann über folgenden Dialog in einer Avtivity eingeschaltet werden:
+		 * 
+		 * Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+    	 * startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+		 */
+		int ret = 0;
 		
 		if(bluetoothAdapter == null){
-			String hallo="test";
+			//Geraet unterstuetzt kein Bluetooth
+			ret = 1;
 		}
+		else{
+			if(!bluetoothAdapter.isEnabled()){
+				//Wenn Bluetooth disabled ist, Bluetooth enablen
+				ret = 2;
+			}
+		}
+		
+		return ret;
 	}
 }
